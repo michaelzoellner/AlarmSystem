62a88566b1d394a8ced3f98f521421475eafe145
Michael Zöllner <mmzoellner@gmail.com>
2020-10-26 11:40:48 +0100

    Update AlarmSystem.cpp

--- main/AlarmSystem.cpp
+++ main/AlarmSystem.cpp
@@ -7,6 +7,24 @@ void AlarmSystem::setState(int newState) {
   
 }
 
+bool AlarmSystem::checkDoor() {
+  val 
+  
+}
+
+bool AlarmSystem::checkMotion() {
+  val pirSensor = digitalRead(m_pirPin);
+  if (val==1) {
+    m_lastMotionDetected = now();
+    return 1; 
+  }  else {
+    return 0;
+  }
+}
+    
+    
+}
+
 void AlarmSystem::stepMachine() {
   
 }
