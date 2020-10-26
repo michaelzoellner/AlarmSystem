cdc06472d6f4e578f4ba66c6fadafae079554383
michaelzoellner <michael.zoellner@rwth-aachen.de>
2020-10-26 21:47:48 +0100

    Bla

--- main/AlarmSystem.cpp
+++ main/AlarmSystem.cpp
@@ -1,30 +1,48 @@
 #include <ESP8266WiFi.h>
 #include "AlarmSystem.h"
+#include <Time.h>
+
+
+AlarmSystem::AlarmSystem()
+
+{
+
+
+}
+
+
 
 void AlarmSystem::setState(int newState) {
   m_oldState = m_state;
   m_state = newState;
-  
+
 }
 
 bool AlarmSystem::checkDoor() {
-  val 
-  
+  val
+
 }
 
 bool AlarmSystem::checkMotion() {
-  val pirSensor = digitalRead(m_pirPin);
-  if (val==1) {
-    m_lastMotionDetected = now();
-    return 1; 
-  }  else {
-    return 0;
+  if (now() - m_bootTime > m_pirWarmUpDuration) {
+    long state = digitalRead(m_pirPin);
+    if (state == HIGH) {
+      Serial.println("Motion detected!")
+      m_lastMotionDetected = now();
+      return true;
+    } else {
+      Serial.println("No motion detected.")
+      return false;
+    }
+  } else {
+    Serial.println("PIR sensor still warming up...")
+    return false;
   }
 }
-    
-    
+
+
 }
 
 void AlarmSystem::stepMachine() {
-  
+
 }
--- main/AlarmSystem.h
+++ main/AlarmSystem.h
@@ -6,21 +6,36 @@ class AlarmSystem
 {
 public:
   AlarmSystem()   // Konstruktor
-  int m_ignoreReachAfterDoorOpen = 2; // minutes
-  int m_noReachDurationForLeave = 15; // minutes
-  int m_wifiLogOnDurationGrant = 30; // seconds
-  bool m_motionDetected;
-  int m_lastMotionDetected;
-  bool m_doorOpen;
-  int m_lastDoorOpen;
-  int m_state;
-  int m_oldState:
+
+  // Setting PINs
+  const int m_trigPin = 12;
+  const int m_echoPin = 13;
+  const int m_pirPin = 5;
+
+  // Parameters
+  const int m_ignoreReachAfterDoorOpen = 2 * 60 * 1000; // 2 minutes
+  const int m_noReachDurationForLeave = 15 * 60 * 1000; // 15 minutes
+  const int m_wifiLogOnDurationGrant = 30 * 1000; // 30 seconds
+
+  // Resident addresses
   Vector<IPAddress> m_addresses;
   m_addresses.push_back(IPAddress(192,168,178,20));
   m_addresses.push_back(IPAddress(192,168,178,125));
 
+  // Operating variables
+  float m_bootTime;
+  bool m_motionDetected;
+  int m_lastMotionDetected;
+  bool m_doorOpen;
+  int m_lastDoorClosed;
+  int m_state;
+  int m_oldState;
+
   void setState(int newState);
   void stepMachine();
+  void checkDoor();
+  void checkMotion();
+  void resetSystem();
 
 private:
 
